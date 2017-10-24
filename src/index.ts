9b506df158d1b199e7e617af5c3f7c0cbb2416ad
Dmitry Sushilov <iamdimka@me.com>
2017-10-24 19:28:37 +0300

    initial

---
+++ src/index.ts
@@ -0,0 +1,98 @@
+import * as path from "path"
+
+export type Signal = "SIGINT" | "SIGTERM" | "SIGUSR1" | "SIGUSR2"
+
+export default class Microservice {
+  isShuttingDown = false
+
+  protected _env = { ...process.env }
+  protected _promises = new Set<Promise<any>>()
+  protected _shutdownCallbacks = [] as Function[]
+
+  run(cb: (m?: Microservice) => void | Promise<void>): this {
+    try {
+      const result = cb(this)
+
+      if (result && result instanceof Promise) {
+        result.catch(e => this.shutdown(e))
+      }
+    } catch (e) {
+      this.shutdown(e)
+    }
+
+    return this
+  }
+
+  extendEnv(...pathSegments: string[]) {
+    const file = path.resolve(...pathSegments)
+    Object.assign(this._env, require(file))
+  }
+
+  env(name: string, defaultValue?: string) {
+    if (this._env.hasOwnProperty(name)) {
+      return this._env[name]
+    }
+
+    if (arguments.length === 1) {
+      throw new Error(`Environment variable "${name}" is required`)
+    }
+
+    return defaultValue
+  }
+
+  promise(cbOrPromise: Function | Promise<any>) {
+    while (typeof cbOrPromise === "function") {
+      cbOrPromise = cbOrPromise()
+    }
+
+    if (cbOrPromise instanceof Promise) {
+      this._promises.add(cbOrPromise as Promise<any>)
+      cbOrPromise.catch(() => null).then(() => this._promises.delete(cbOrPromise as Promise<any>))
+    }
+  }
+
+  handleSignals(...signals: Signal[]) {
+    for (const signal of signals) {
+      process.on(signal, () => this.shutdown(signal))
+    }
+  }
+
+  async shutdown(reason?: Signal | Error) {
+    if (this.isShuttingDown) {
+      return
+    }
+
+    if (typeof reason === "string") {
+      console.info(`Going down: Got signal ${reason}`)
+    } else if (reason && reason instanceof Error) {
+      console.error(`Going down: Unhandled error ${reason.message}`)
+    }
+
+    let code = 0
+
+    this._shutdownCallbacks.push(() => Promise.all(Array.from(this._promises)))
+
+    while (this._shutdownCallbacks.length) {
+      try {
+        const item = this._shutdownCallbacks.pop() as Function
+        await item()
+      } catch (e) {
+        code = 1
+        console.error(e)
+      }
+    }
+
+    process.exit(code)
+  }
+
+  registerShutdownCallback(fn: Function) {
+    this._shutdownCallbacks.push(fn)
+    return this
+  }
+}
+
+export function microservice(fn: (m?: Microservice) => void | Promise<void>): Microservice {
+  const m = new Microservice()
+  m.run(fn)
+  return m
+}
